956ea2037a75b478fb55e7451a3e35d80208204c
aalohaa <khusainov.alizhan@bk.ru>
2023-01-19 01:31:24 +0600

    UI Design

--- app/src/main/java/com/example/booksapp/MainActivity.kt
+++ app/src/main/java/com/example/booksapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.booksapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.booksapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //handle click, login
+        binding.loginBtn.setOnClickListener {
+            // will do later
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        //hand click, skip and continue to main screen
+        binding.skipBtn.setOnClickListener {
+            //will do later
+            startActivity(Intent(this, DashboardUserActivity::class.java))
+        }
+
+
     }
 }
