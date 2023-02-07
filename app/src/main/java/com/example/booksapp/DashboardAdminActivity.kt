f1000f77d0c11bd225bc2f638831563e69bdf562
aalohaa <khusainov.alizhan@bk.ru>
2023-02-08 04:25:01 +0600

    Show categories and searching.

--- app/src/main/java/com/example/booksapp/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/booksapp/DashboardAdminActivity.kt
@@ -3,8 +3,15 @@ package com.example.booksapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import java.lang.Exception
 import com.example.booksapp.databinding.ActivityDashboardAdminBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class DashboardAdminActivity : AppCompatActivity() {
 
@@ -14,6 +21,12 @@ class DashboardAdminActivity : AppCompatActivity() {
     //firebase auth
     private lateinit var firebaseAuth: FirebaseAuth
 
+
+    //arraylist to hold categories
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+    //adapter
+    private lateinit var adapterCategory: AdapterCategory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
@@ -22,6 +35,29 @@ class DashboardAdminActivity : AppCompatActivity() {
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        loadCategories()
+
+        //search
+        binding.searchEt.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //called as and when user type anything
+                try {
+                    adapterCategory.filter.filter(s)
+                }
+                catch (e: Exception){
+
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
 
         //handle click, logout
         binding.logoutBtn.setOnClickListener {
@@ -34,6 +70,35 @@ class DashboardAdminActivity : AppCompatActivity() {
             startActivity(Intent(this, CategoryAddActivity::class.java))
         }
 
+    }
+
+    private fun loadCategories() {
+        //init arraylist
+        categoryArrayList = ArrayList()
+
+        //get all categories from firebase database... Firebase DB > Categories
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clear list before starting adding data into it
+                categoryArrayList.clear()
+                for (ds in snapshot.children){
+                    //get data as model
+                    val model = ds.getValue(ModelCategory::class.java)
+
+                    //add to arraylist
+                    categoryArrayList.add(model!!)
+                }
+                //setup adapter
+                adapterCategory = AdapterCategory(this@DashboardAdminActivity, categoryArrayList)
+                //set adapter to recyclerview
+                binding.categoriesRv.adapter = adapterCategory
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
     }
 
     private fun checkUser() {
