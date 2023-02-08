28f8a93cb2be75971540902fbf52cb30a5790d0b
aalohaa <khusainov.alizhan@bk.ru>
2023-02-08 16:37:15 +0600

    pdf add activity

--- app/src/main/java/com/example/booksapp/PdfAddActivity.kt
+++ app/src/main/java/com/example/booksapp/PdfAddActivity.kt
@@ -1,20 +1,49 @@
 package com.example.booksapp
 
+import android.app.ProgressDialog
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.booksapp.databinding.ActivityPdfAddBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class PdfAddActivity : AppCompatActivity() {
 
     //setup view binding activity_pdf_add -> ActivityPdfAddBinding
-
+    private lateinit var binding:ActivityPdfAddBinding
 
     //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     //progress dialog
+    private lateinit var progressDialog: ProgressDialog
+
     //arraylist to hold pdf categories
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+
     //uri of picked pdf
+    private var pdfUri: Uri? = null
+
+    //TAG
+    private val TAG = "PDF_ADD_TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pdf_add)
+        binding = ActivityPdfAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        loadPdfCategories()
+
+        //setup progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+    }
+
+    private fun loadPdfCategories() {
+
     }
 }
