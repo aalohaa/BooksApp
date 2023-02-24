076e8bdfc8dcff824edc106f4d81e979b793e96c
aalohaa <khusainov.alizhan@bk.ru>
2023-02-24 10:34:50 +0600

    Load pdf/Book category

--- app/src/main/java/com/example/booksapp/AdapterPdfAdmin.kt
+++ app/src/main/java/com/example/booksapp/AdapterPdfAdmin.kt
@@ -1,16 +1,58 @@
 package com.example.booksapp
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.booksapp.databinding.RowPdfAdminBinding
 
-class AdapterPdfAdmin {
+class AdapterPdfAdmin :RecyclerView.Adapter<AdapterPdfAdmin.HolderPdfAdmin>{
+
+    //context
+    private var context: Context
+    //arraylist to hold pdfs
+    private var pdfArraylist: ArrayList<ModelPdf>
 
     //viewBinding
     private lateinit var binding:RowPdfAdminBinding
 
+    //constructor
+    constructor(context: Context, pdfArraylist: ArrayList<ModelPdf>) : super() {
+        this.context = context
+        this.pdfArraylist = pdfArraylist
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderPdfAdmin {
+        //bind/inflate layout row_pdf_admin.xml
+        binding = RowPdfAdminBinding.inflate(LayoutInflater.from(context), parent, false)
+
+        return HolderPdfAdmin(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: HolderPdfAdmin, position: Int) {
+        /*Get data, Set data, Handle click & etc*/
+
+        //get data
+        val model = pdfArraylist[position]
+        val pdfId = model.id
+        val categoryId = model.categoryId
+        val title = model.title
+        val description = model.description
+        val pdfUrl = model.url
+        val timestamp = model.timestamp
+        //convert timestamp to dd//MM/yyyy format
+
+        //
+    }
+
+    override fun getItemCount(): Int {
+        return pdfArraylist.size //items count
+    }
+
     /* View holder class for row_pdf_admin.xml*/
-    inner class HolderPdfAdmin(itemView: View) : ViewHolder(itemView){
+    inner class HolderPdfAdmin(itemView: View) : RecyclerView.ViewHolder(itemView){
         // UI views of row_pdf_admin.xml
         val pdfView = binding.pdfView
         val progressBar = binding.progressBar
